b6289e7057fcf352026f8d29747eb7d36a1638d8
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-07 16:51:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let rotationAngle = 0;
+
+const rotatingElement = document.getElementById('line');
+
+function rotateElement(){
+	rotationAngle += 2;
+	rotatingElement.style.transform = `rotate(${rotationAngle}deg)`;
+}
+
+setInterval(rotateElement, 20);
